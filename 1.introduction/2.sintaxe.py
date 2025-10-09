4fcd028189a6fce33325eb47a3eb3a8cc109d44e
Maicon Silva <maiconrs95@gmail.com>
2025-10-09 08:18:02 -0300

    Hello World

---
+++ 1.introduction/2.sintaxe.py
@@ -0,0 +1,27 @@
+# First Comment
+'''
+Second Comment
+Third Comment
+'''
+
+'''
+Fourth Comment
+Fifth Comment
+Sixth Comment
+'''
+
+age = 30
+name = "Maico"
+last_name = "Silva"
+text = "Hello, " + name + " " + \
+    last_name
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+def print_text():
+    print(text)
+
+print_text()
